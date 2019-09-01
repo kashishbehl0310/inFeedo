4e8273497bb6eb6a9d6ef197892d03948e5c9658
Kashish <kashish.behl@holidayiq.com>
2019-09-01 14:17:06 +0530

    Deploying to heroku

--- server/server.js
+++ server/server.js
@@ -1,13 +1,15 @@
 const express = require('express');
-const path = require('path');  
+const path = require('path');
 const app = express();
 const publicPath = path.join(__dirname, '..', 'dist');
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000 ;
 
-app.use(express.static(publicPath));
+app.use(express.static(publicPath))
+
 app.get('/*', (req, res) => {
     res.sendFile('index.html', {root: publicPath})
 })
+
 app.listen(port, () => {
-    console.log("server is up")
+    console.log("Server is up and running")
 })
